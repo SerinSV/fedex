7e14d1352a3ae5fef8998507ebc26825f25c9621
SerinSV <109526214+SerinSV@users.noreply.github.com>
2024-04-18 13:26:20 +0530

    publish_url

--- scripts/core/utils/mqtt_subscriber.py
+++ scripts/core/utils/mqtt_subscriber.py
@@ -5,7 +5,7 @@ from loguru import logger
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
-    client.subscribe("Fedex_#")
+    client.subscribe("Fedex/#")
 
 
 def on_message(client, userdata, msg):
